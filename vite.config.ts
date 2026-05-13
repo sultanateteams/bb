b0452bc2ef374fe6b76302f3a9f9d1d710a55805
Abdug'ani <sultanateteam@gmail.com>
2026-05-13 10:07:37 +0500

    Configure base path in Vite config
    Set base path to prevent MIME type errors.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // BASE yo'lini ko'rsatish MIME xatosini oldini oladi
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
